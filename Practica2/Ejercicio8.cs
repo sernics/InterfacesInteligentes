dc86a78a64b808d449a706da2fda3418aa06238c
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-10-07 15:49:13 +0100

    Arreglado error cubo más cercano

--- Practica2/Ejercicio8.cs
+++ Practica2/Ejercicio8.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ejercicio8 : MonoBehaviour {
+  public int aumento = 3;
+
   void Start() {
-    
+    aumentarMasCercana();
+  }
+
+  void aumentarMasCercana() {
+    GameObject cubo = GameObject.FindWithTag("cubo");
+    GameObject[] esferas = GameObject.FindGameObjectsWithTag("tipo2");
+    GameObject cercana = null;
+    float distancia = float.MaxValue;
+    foreach (GameObject esfera in esferas) {
+      float d = Vector3.Distance(cubo.transform.position, esfera.transform.position);
+      if (d < distancia) {
+        distancia = d;
+        cercana = esfera;
+      }
+    }
+    // Aumentar la altura de la esfera más cercana
+    Vector3 pos = cercana.transform.position;
+    pos.y += aumento;
+    cercana.transform.position = pos;
   }
 
   void CambiarColor() {
